b2a6cf21473bf9356c2d2beef4def91f04590e7a
imtaha935-ui <imtaha935@gmail.com>
2026-04-08 12:07:43 +0500

    Update agent-client.tsx

--- components/embed-popup/agent-client.tsx
+++ components/embed-popup/agent-client.tsx
@@ -142,9 +142,10 @@ function AgentClient({ appConfig }: EmbedFixedAgentClientProps) {
                 initial={{ opacity: 1 }}
                 animate={{ opacity: error === null ? 1 : 0 }}
                 transition={{
-                  type: 'linear',
-                  duration: 0.2,
-                }}
+                ease: 'linear',
+                duration: 0.2,
+                   }}
+
                 disabled={!popupOpen}
                 sessionStarted={popupOpen}
                 onEmbedError={setError}
