8213703c1df67d20defd7b9fdc290b5410d8757b
imtaha935-ui <imtaha935@gmail.com>
2026-04-08 11:47:02 +0500

    Update route.ts

--- app/api/connection-details/route.ts
+++ app/api/connection-details/route.ts
@@ -1,4 +1,4 @@
-export const runtime = 'edge';
+export const runtime = 'nodejs'; 
 import { NextResponse } from 'next/server';
 import { AccessToken, type AccessTokenOptions, type VideoGrant } from 'livekit-server-sdk';
 import { RoomConfiguration } from '@livekit/protocol';
