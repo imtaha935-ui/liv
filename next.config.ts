1d1d9c5929fd3f4b649f0956489c645e122abf56
imtaha935-ui <imtaha935@gmail.com>
2026-04-08 14:12:58 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   async headers() {
     const iframeHeaders = [
       { key: 'X-Frame-Options', value: 'ALLOWALL' },
